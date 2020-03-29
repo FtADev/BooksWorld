748eae0b1a099b919a061961b78896b21e7fa094
ftadev <akhlaghi.fatemeh@gmail.com>
2020-03-29 19:17:26 +0430

    + Fragment Class

---
+++ app/src/main/java/com/ftadev/booksworld/AllBooksFragment.kt
@@ -0,0 +1,25 @@
+package com.ftadev.booksworld
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class AllBooksFragment : Fragment() {
+    lateinit var textView: TextView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_book, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/ftadev/booksworld/MainActivity.kt
+++ app/src/main/java/com/ftadev/booksworld/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.ftadev.booksworld
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private val fragmentManager = supportFragmentManager
+    private val firstFragment = AllBooksFragment()
+    private val secondFragment = CategoryBooksFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.book_list_fragment, firstFragment)
+        fragmentTransaction.commit()
+
     }
+
+
+    private fun allBookClick() {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.book_list_fragment, firstFragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun categoryBookClick() {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.book_list_fragment, secondFragment)
+        fragmentTransaction.commit()
+    }
+
 }
