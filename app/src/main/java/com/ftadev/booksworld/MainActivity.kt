4b66fcf6f7053dfd6396010e9c9e62c456fb4806
ftadev <akhlaghi.fatemeh@gmail.com>
2020-03-30 01:58:03 +0430

    Gradient status bar

--- app/src/main/java/com/ftadev/booksworld/MainActivity.kt
+++ app/src/main/java/com/ftadev/booksworld/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ftadev.booksworld
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.appbar.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // For gradient status bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        }
+
         setContentView(R.layout.activity_main)
 
         val fragmentTransaction = fragmentManager.beginTransaction()
