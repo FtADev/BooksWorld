fa85513aafbd4bba3ae95f47876012af0bc9a357
ftadev <akhlaghi.fatemeh@gmail.com>
2020-04-01 18:29:04 +0430

    + BookInfo APIServices

--- app/src/main/java/com/ftadev/booksworld/api/ApiService.kt
+++ app/src/main/java/com/ftadev/booksworld/api/ApiService.kt
@@ -1,11 +1,19 @@
 package com.ftadev.booksworld.api
 
+import com.ftadev.booksworld.model.BookImageModel
 import com.ftadev.booksworld.model.BookModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
-    @GET("Book/")
-    suspend fun getBooksBySubject() : Response<List<BookModel>>
+    @GET("book/")
+    suspend fun getAllBooks() : Response<List<BookModel>>
+
+    @GET("book/image")
+    suspend fun getBookImages() : Response<List<BookImageModel>>
+
+    @GET("book/{id}")
+    suspend fun getBookInfo(@Path("id") id: Int) : Response<BookModel>
 }
