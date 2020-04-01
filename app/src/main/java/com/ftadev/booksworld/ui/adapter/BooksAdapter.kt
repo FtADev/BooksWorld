58b97a899c9466db73874f23c6b62f594da7a393
ftadev <akhlaghi.fatemeh@gmail.com>
2020-04-01 18:30:36 +0430

    Change names in adapter

--- app/src/main/java/com/ftadev/booksworld/ui/adapter/BooksAdapter.kt
+++ app/src/main/java/com/ftadev/booksworld/ui/adapter/BooksAdapter.kt
@@ -6,14 +6,14 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.ftadev.booksworld.R
-import com.ftadev.booksworld.model.BookModel
+import com.ftadev.booksworld.model.BookImageModel
 import com.squareup.picasso.Picasso
 
 class BookAdapter : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
 
-    private var bookList: ArrayList<BookModel> = ArrayList()
+    private var bookList: ArrayList<BookImageModel> = ArrayList()
 
-    fun setBooks(books: List<BookModel>) {
+    fun setBooks(books: List<BookImageModel>) {
 
         bookList.addAll(books)
         notifyDataSetChanged()
@@ -35,20 +35,11 @@ class BookAdapter : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
 
         val book = bookList[position]
-
-//        holder.bookId.text = book.id.toString()
-//        holder.name.text = book.title
-//        holder.bookName.text = book.bookname
-//        holder.email.text = book.email
         Picasso.get().load(book.photo).into(holder.image);
     }
 
     class BookViewHolder(parent: View) : RecyclerView.ViewHolder(parent) {
 
-//        val bookId = parent.findViewById<TextView>(R.id.bookId)
-//        val name = parent.findViewById<TextView>(R.id.name)
-//        val bookName = parent.findViewById<TextView>(R.id.bookName)
-//        val email = parent.findViewById<TextView>(R.id.email)
         val image = parent.findViewById<ImageView>(R.id.image)
 
     }
