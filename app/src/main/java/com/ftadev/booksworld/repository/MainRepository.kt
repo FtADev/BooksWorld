e18855c04db920a12e0e358e337c1bdb9393df6d
ftadev <akhlaghi.fatemeh@gmail.com>
2020-04-01 18:29:48 +0430

    Change name and + BookInfo Repository

--- app/src/main/java/com/ftadev/booksworld/repository/MainRepository.kt
+++ app/src/main/java/com/ftadev/booksworld/repository/MainRepository.kt
@@ -3,6 +3,7 @@ package com.ftadev.booksworld.repository
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.ftadev.booksworld.api.RetrofitManager
+import com.ftadev.booksworld.model.BookImageModel
 import com.ftadev.booksworld.model.BookModel
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
@@ -11,51 +12,83 @@ class MainRepository {
 
     private val apiService = RetrofitManager.apiService
 
-    val booksSuccessLiveData = MutableLiveData<List<BookModel>>()
-    val booksFailureLiveData = MutableLiveData<Boolean>()
+    val booksImageSuccessLiveData = MutableLiveData<List<BookImageModel>>()
+    val booksImageFailureLiveData = MutableLiveData<Boolean>()
+
+    val bookSuccessLiveData = MutableLiveData<BookModel>()
+    val bookFailureLiveData = MutableLiveData<Boolean>()
 
     /*
     this fun is suspend fun means it will execute in different thread
      */
-    suspend fun getBooks() {
+    suspend fun getBooksImage() {
 
         try {
 
             //here api calling became so simple just 1 line of code
             //there is no callback needed
 
-            val response = apiService.getBooksBySubject()
+            val response = apiService.getBookImages()
 
             Log.d(TAG, "$response")
 
             if (response.isSuccessful) {
                 Log.d(TAG, "SUCCESS")
                 Log.d(TAG, "${response.body()}")
-                booksSuccessLiveData.postValue(response.body())
+                booksImageSuccessLiveData.postValue(response.body())
 
             } else {
                 Log.d(TAG, "FAILURE")
                 Log.d(TAG, "${response.body()}")
-                booksFailureLiveData.postValue(true)
+                booksImageFailureLiveData.postValue(true)
             }
 
         } catch (e: UnknownHostException) {
             Log.e(TAG, e.message)
             //this exception occurs when there is no internet connection or host is not available
             //so inform book that something went wrong
-            booksFailureLiveData.postValue(true)
+            booksImageFailureLiveData.postValue(true)
         } catch (e: SocketTimeoutException) {
             Log.e(TAG, e.message)
             //this exception occurs when time out will happen
             //so inform book that something went wrong
-            booksFailureLiveData.postValue(true)
+            booksImageFailureLiveData.postValue(true)
         } catch (e: Exception) {
             Log.e(TAG, e.message)
             //this is generic exception handling
             //so inform book that something went wrong
-            booksFailureLiveData.postValue(true)
+            booksImageFailureLiveData.postValue(true)
         }
 
+    }
+
+    suspend fun getBookInfo(id: Int) {
+        try {
+            val response = apiService.getBookInfo(id)
+
+            Log.d(TAG, "$response")
+
+            if (response.isSuccessful) {
+                Log.d(TAG, "SUCCESS")
+                Log.d(TAG, "${response.body()}")
+                bookSuccessLiveData.postValue(response.body())
+
+            } else {
+                Log.d(TAG, "FAILURE")
+                Log.d(TAG, "${response.body()}")
+                booksImageFailureLiveData.postValue(true)
+            }
+
+        } catch (e: UnknownHostException) {
+            Log.e(TAG, e.message)
+            booksImageFailureLiveData.postValue(true)
+        } catch (e: SocketTimeoutException) {
+            Log.e(TAG, e.message)
+            booksImageFailureLiveData.postValue(true)
+        } catch (e: Exception) {
+            Log.e(TAG, e.message)
+            booksImageFailureLiveData.postValue(true)
+        }
     }
 
     companion object {
