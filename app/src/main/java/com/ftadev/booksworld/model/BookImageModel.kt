ca3ba96b9b791c1ee45302738ec148fdae49b31d
ftadev <akhlaghi.fatemeh@gmail.com>
2020-04-02 13:15:20 +0430

    + id to model

--- app/src/main/java/com/ftadev/booksworld/model/BookImageModel.kt
+++ app/src/main/java/com/ftadev/booksworld/model/BookImageModel.kt
@@ -1,5 +1,6 @@
 package com.ftadev.booksworld.model
 
 data class BookImageModel(
+    val id: Int,
     val photo: String
 )
--- app/src/main/java/com/ftadev/booksworld/model/BookModel.kt
+++ app/src/main/java/com/ftadev/booksworld/model/BookModel.kt
@@ -1,10 +1,10 @@
 package com.ftadev.booksworld.model
 
 data class BookModel(
+    val id: Int,
     val author: String,
     val category: String?,
     val descr: String,
-    val id: Int,
     val name: String,
     val photo: String,
     val publisher: String?,
