1ee244a46873862d63905194564bbc3471add892
ftadev <akhlaghi.fatemeh@gmail.com>
2020-04-07 16:30:21 +0430

    Add db methods to ViewModel

--- app/src/main/java/com/ftadev/booksworld/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ftadev/booksworld/ui/viewmodel/MainViewModel.kt
@@ -1,28 +1,40 @@
 package com.ftadev.booksworld.ui.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.ftadev.booksworld.model.BookModel
+import com.ftadev.booksworld.repository.DBRepository
 import com.ftadev.booksworld.repository.MainRepository
 import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val mainRepository =
-        MainRepository()
+    private val mainRepository = MainRepository()
+    private var dbRepository: DBRepository = DBRepository(application)
 
     val booksSuccessLiveData = mainRepository.booksImageSuccessLiveData
     val booksFailureLiveData = mainRepository.booksImageFailureLiveData
+
     val bookInfoSuccessLiveData = mainRepository.bookSuccessLiveData
     val bookInfoFailureLiveData = mainRepository.bookFailureLiveData
 
-    fun getBooksImage() {
-        //this is coroutine viewmodel scope to call suspend fun of repo
-        viewModelScope.launch { mainRepository.getBooksImage() }
+    val bookmarkSuccessLiveData = dbRepository.bookmarkSuccessLiveData
+    val bookmarkFailureLiveData = dbRepository.bookmarkFailureLiveData
 
+    fun getBooksImage() {
+        viewModelScope.launch { mainRepository.getBooksImage() }
     }
 
     fun getBookInfo(id: Int) {
         viewModelScope.launch { mainRepository.getBookInfo(id) }
+    }
 
+    fun getBookmarkBooks() {
+        viewModelScope.launch { dbRepository.getBook() }
+    }
+
+    fun addBookmark(book: BookModel) {
+        viewModelScope.launch { dbRepository.addBook(book) }
     }
 }
