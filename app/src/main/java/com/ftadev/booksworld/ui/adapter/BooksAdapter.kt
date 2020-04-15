0b7eee3f1bc5beeb6f78d62e50ff79a57dc19641
ftadev <akhlaghi.fatemeh@gmail.com>
2020-04-15 23:46:36 +0430

    Changing BookAdapter

--- app/src/main/java/com/ftadev/booksworld/ui/adapter/BooksAdapter.kt
+++ app/src/main/java/com/ftadev/booksworld/ui/adapter/BooksAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -14,9 +15,12 @@ import com.ftadev.booksworld.paging.DiffUtilCallBack
 import com.ftadev.booksworld.ui.activity.BookActivity
 import kotlinx.android.synthetic.main.card_book.view.*
 
-class BookAdapter : PagedListAdapter<BookImageModel, BookAdapter.BookViewHolder>(
-    DiffUtilCallBack()
-) {
+class BookAdapter(var itemClickListener: ((BookImageModel) -> Unit)? = null) :
+    PagedListAdapter<BookImageModel, BookAdapter.BookViewHolder>(DiffUtilCallBack()) {
+
+    class BookViewHolder(parent: View) : RecyclerView.ViewHolder(parent) {
+        val photo: ImageView = parent.findViewById(R.id.image)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.card_book, parent, false)
@@ -24,22 +28,17 @@ class BookAdapter : PagedListAdapter<BookImageModel, BookAdapter.BookViewHolder>
     }
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
-        getItem(position)?.let { holder.bindBook(it) }
-    }
-
-    class BookViewHolder(parent: View) : RecyclerView.ViewHolder(parent) {
-
-        fun bindBook(book: BookImageModel) {
-            itemView.run {
-                animation =
-                    AnimationUtils.loadAnimation(context, R.anim.item_animation_fall_down)
-                Glide.with(this).load(book.photo).into(image) // It will have circular corner depends on its parent card xml
-                setOnClickListener {
-                    val intent = Intent(context, BookActivity::class.java)
-                    intent.putExtra("ID", book.id)
-                    intent.putExtra("isComeFromDB", false)
-                    context.startActivity(intent)
-                }
+        val item = getItem(position)
+        holder.itemView.run {
+            animation =
+                AnimationUtils.loadAnimation(context, R.anim.item_animation_fall_down)
+            Glide.with(this).load(item?.photo).into(holder.photo) // It will have circular corner depends on its parent card xml
+            setOnClickListener {
+                if (item != null) itemClickListener?.invoke(item)
+//                val intent = Intent(context, BookActivity::class.java)
+//                    intent.putExtra("ID", item?.id)
+//                    intent.putExtra("isComeFromDB", false)
+//                    context.startActivity(intent)
             }
         }
     }
