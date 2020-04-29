3825d4765536e747a210aa71649dee142c209476
ftadev <akhlaghi.fatemeh@gmail.com>
2020-04-30 02:40:06 +0430

    Check marked book from db

--- app/src/main/java/com/ftadev/booksworld/db/BookDao.kt
+++ app/src/main/java/com/ftadev/booksworld/db/BookDao.kt
@@ -17,4 +17,7 @@ interface BookDao {
     @Query("SELECT * FROM book WHERE id = :id")
     fun getBook(id: Int): LiveData<BookModel>
 
+    @Query("SELECT EXISTS(SELECT 1 FROM book WHERE id = :bookId LIMIT 1)")
+    fun isMarked(bookId: Int): LiveData<Boolean>
+
 }
